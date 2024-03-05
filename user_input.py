70a7b9ec1c8291447dca2e203494c30d4eff0a2f
ocheinstein1 <ochegabasphysics@gmail.com>
2024-03-05 11:28:02 +0100

    my first commit in python

--- user_input.py
+++ user_input.py
@@ -1,5 +1,5 @@
 name = input("Please, enter your name:")
 age  = input("Please, enter uour age:")
 location = input("Please, enter your location:")
-print("Hello ", +name+ "you are " +age+ "years old and live in ", +location )
+print("Hello, " +name+ "you are " +age+ "years old and live in ", +location )
 
